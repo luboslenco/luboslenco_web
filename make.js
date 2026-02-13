24e59c5f9621c30801fcba079ffef2ee86b71acf
luboslenco <lubos.lenco@gmail.com>
2026-02-13 17:30:41 +0100

    Sync

--- make.js
+++ make.js
@@ -5,26 +5,26 @@ let fs = require('fs');
 	let rss = `<?xml version="1.0" encoding="UTF-8" ?>
 	<rss version="2.0">
 	<channel>
-		<title>lubos-lenco-blog</title>
-		<link>https://luboslenco.com/blog</link>
-		<description>3D Content Creation Tools</description>
+		<title>lubos-lenco</title>
+		<link>https://luboslenco.com</link>
+		<description>Lubos' Log</description>
 	`;
 
-	let news = fs.readFileSync("blog.html", "utf8");
-	let h3s = news.split(`<h3>`);
+	let index = fs.readFileSync("index.html", "utf8");
+	let divs = index.split(`<div id="rss">`);
+	divs.shift();
 	let items = [];
-	h3s.shift();
-	h3s.shift();
-	for (let h3 of h3s) {
-		items.push(h3.split("</h3>")[0]);
+	for (let div of divs) {
+		items.push(div.split("</div>")[0]);
 	}
 
 	for (let item of items) {
 		rss += `
 			<item>
-	    		<title>lubos-lenco-blog</title>
-	    		<link>https://luboslenco.com/blog</link>
-	    		<description>${item}</description>
+	    		<title>Lubos' Log</title>
+	    		<link>https://luboslenco.com</link>
+	    		<description>${item}
+	    		</description>
 			</item>
 
 		`;
